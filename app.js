9ad5ae89374696bf82b0043ebe7729d7dd5add57
SaiSwethaBodlapati <saiswethabodlapati@gmail.com>
2024-03-27 00:57:54 -0500

    Added a new pick

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var dogRouter = require('./routes/dog');
+var catRouter = require('./routes/cat');
 var gridRouter = require('./routes/grid');
 var pickRouter = require('./routes/pick');
 
@@ -24,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/dog', dogRouter);
+app.use('/cat', catRouter);
 app.use('/grid', gridRouter);
 app.use('/pick', pickRouter);
 
